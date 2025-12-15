9a2b0394fd452520ebda14fb39fe354ca32ad15d
Armin Golzar <armingolzar78@gmail.com>
2025-12-15 19:38:54 +0330

    added preprocess to data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -36,7 +36,7 @@ train_idx, test_idx = train_test_split(range(len(labels)), test_size=0.2, random
 def select_split(tensor, indices):
     return tf.gather(tensor, indices)
 
-train_paths = {
+train_data = {
                 "tabular" : select_split(tabular_data, train_idx),
                 "bathroom" : select_split(bathroom_paths, train_idx),
                 "bedroom" : select_split(bedroom_paths, train_idx),
@@ -46,7 +46,7 @@ train_paths = {
 
 train_labels = select_split(labels, train_idx)
 
-test_paths = {
+test_data = {
                 "tabular" : select_split(tabular_data, test_idx),
                 "bathroom" : select_split(bathroom_paths, test_idx),
                 "bedroom" : select_split(bedroom_paths, test_idx),
@@ -55,6 +55,35 @@ test_paths = {
 }
 
 test_labels = select_split(labels, test_idx)
+
+# Normalization
+
+tab_mean = tf.reduce_mean(train_data["tabular"], axis=0)
+tab_std = tf.math.reduce_std(train_data["tabular"], axis=0)
+tab_std = tf.where(tab_std == 0, 1.0, tab_std)
+
+label_min = tf.reduce_min(train_labels)
+label_max = tf.reduce_max(train_labels)
+label_range = tf.where(label_max - label_min == 0, 1.0, label_max - label_min)
+
+# Preprocessing Functions
+
+def load_image(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (32, 32))
+    return tf.cast(img, tf.float32) / 255.0
+
+def prerocess(tabular, bath, bed, kitch, front, label):
+    bath = load_image(bath)
+    bed = load_image(bed)
+    kitch = load_image(kitch)
+    front = load_image(front)
+
+    tabular = (tabular - tab_mean) / (tab_std + 1e-8)
+    label = (label - label_min) / (label_range + 1e-8)
+
+    return {"tabular" : tabular, "bathroom" : bath, "bedroom" : bed, "kitchen" : kitch, "frontal" : front}, label
 
 
 
