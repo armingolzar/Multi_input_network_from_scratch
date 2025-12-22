7f31f095919af999a14d9081e4814fa746c122f1
Armin Golzar <armingolzar78@gmail.com>
2025-12-22 18:55:51 +0330

    CustomDense implemented

--- src/model.py
+++ src/model.py
@@ -4,14 +4,37 @@ from tensorflow.kerase.models import Model
 from tensorflow.keras.losses import Loss
 
 
-class CUSTOMRELU(Layer):
+class CustomRelu(Layer):
 
-    def __init__(self, name="CUSTOMRELU"):
+    def __init__(self, name="CustomRelu"):
         super().__init__(name=name)
 
     def call(self, inputs):
         return tf.maximum(inputs, 0.0)
     
+
+class CustomDense(Layer):
+
+    def __init__(self, units, name="CustomDense"):
+        super().__init__(name=name)
+        self.units = units
+
+    def build(self, input_shape):
+
+        in_features = input_shape[-1]
+
+        self.W = self.add_weight(name="WEIGHTS", shape=(in_features, self.units), initializer="he_normal", trainable=True)
+
+        self.B = self.add_weight(name="BIAS", shape=(self.units,), initializer="zeros", trainable=True)
+
+    def call(self, inputs):
+
+        output = tf.matmul(inputs, self.W) + self.B
+        return output
     
+    def get_config(self):
+        config = super().get_config()
+        config.update({"units" : self.units})
+        return config
 
 
