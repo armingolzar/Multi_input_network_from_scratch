09d32adffd698b425f93bcd0d5bd4ef974bda490
Armin Golzar <armingolzar78@gmail.com>
2026-01-05 18:19:06 +0330

    training with fit and callbacks is done

--- src/train.py
+++ src/train.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
-from tensorflow.keras.callbacks import (ModelCheckpoint, EarlyStopping, ReaduceLROnPlateau, TensorBoard, CSVLogger)
+from tensorflow.keras.callbacks import (ModelCheckpoint, EarlyStopping, ReaduceLROnPlateau, CSVLogger)
 from data_loader import data_prepration
 from utils import training_curve
 from model import HousePriceCusomModel, SmoothL1Loss
 
+callbacks = [ModelCheckpoint(filepath="..\\models\\checkpoints\\best_model.keras", monitor="val_loss", save_best_only=True, verbose=1), EarlyStopping(monitor="val_loss", patience=10, restore_best_weights=True,
+                            verbose=1), ReaduceLROnPlateau(monitor="val_loss", factor=0.3, patience=5, min_lr=1e-6, verbose=1), CSVLogger(filename="..\\model\\log\\train_log.csv", append=False)]
 
 model = HousePriceCusomModel()
+train_ds, val_ds = data_prepration()
 
 model.compile(
                 loss=SmoothL1Loss(delta=1.0),
                 optimizer=tf.keras.optimizer.Adam(learning_rate=1e-3),
                 metrics=["mae"]
 )
+
+history = model.fit(train_ds, epochs=50, validation_data=val_ds, callbacks=callbacks)
+
+training_curve(history)
 
 
--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,7 @@ import matplotlib.pyplot as plt
 
 def training_curve(history):
 
-    acc = history.history["accuracy"]
-    val_acc = history.history["val_accuracy"]
-
+    
     loss = history.history["loss"]
     val_loss = history.history["val_loss"]
     epochs = range(len(loss))
@@ -12,7 +10,6 @@ def training_curve(history):
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
     plt.plot(epochs, loss, color="red", label="Training Loss")
-    plt.plot(epochs, val_loss, color="blue", label="Validation Loss")
     plt.title("Loss")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
@@ -20,11 +17,10 @@ def training_curve(history):
     plt.grid(True)
 
     plt.subplot(1, 2, 2)
-    plt.plot(epochs, acc, color="red", label="Training Accuracy")
-    plt.plot(epochs, val_acc, color="blue", label="Validation Accuracy")
-    plt.title("Accuracy")
+    plt.plot(epochs, val_loss, color="blue", label="Validation Loss")
+    plt.title("Val_loss")
     plt.xlabel("Epoch")
-    plt.ylabel("Accuracy")
+    plt.ylabel("Val_loss")
     plt.legend()
     plt.grid(True)
 
